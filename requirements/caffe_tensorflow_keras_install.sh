0add808bddc6c9f2efe11a906992992367a36067
Ram81 <ramramrakhya81@gmail.com>
2018-05-10 13:53:43 +0530

    Minor Fixes

--- requirements/caffe_tensorflow_keras_install.sh
+++ requirements/caffe_tensorflow_keras_install.sh
@@ -1,12 +1,12 @@
 # Generic dependencies
 echo "Installing generic dependencies"
-sudo apt-get install git libatlas-base-dev python-protobuf python-numpy python-scipy python-h5py unzip make libblas-dev liblapack-dev libatlas-base-dev gfortran python-pip python-dev
+sudo apt-get -y install git libatlas-base-dev python-protobuf python-numpy python-scipy python-h5py unzip make libblas-dev liblapack-dev libatlas-base-dev gfortran python-pip python-dev
 pip install numpy scipy scikit-image
 
 #Caffe specific dependencies
 echo "Installing caffe specific dependencies"
-sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler cmake
-sudo apt-get install --no-install-recommends libboost-all-dev
+sudo apt-get -y install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev libgflags-dev libgoogle-glog-dev liblmdb-dev protobuf-compiler cmake
+sudo apt-get -y install --no-install-recommends libboost-all-dev
 
 if [ ! -d $HOME/caffe/caffe ]; then
 		#Download caffe
@@ -26,14 +26,14 @@ if [ ! -d $HOME/caffe/caffe ]; then
 		cd build
 		cmake -DCPU_ONLY=1 -DBUILD_python_layer=1 ..
 		make -j"$(nproc)"
-		
+
 		echo "export PYTHONPATH=$PYTHONPATH:$HOME/caffe/caffe/python" >> ~/.bash_profile
 fi
 echo "#################### Caffe Install Complete! ####################"
 
 echo "Installing Tensorflow dependencies"
-sudo apt-get install python-pip python-dev google-perftools
-export LD_PRELOAD="/usr/lib/libtcmalloc.so.4" 
+sudo apt-get -y install python-pip python-dev google-perftools
+export LD_PRELOAD="/usr/lib/libtcmalloc.so.4"
 
 echo "Installing Tensorflow"
 pip install tensorflow==1.4.1
