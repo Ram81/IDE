b54d72f44aae877509f5d7468776efd6aacc35f4
Ram81 <ramramrakhya81@gmail.com>
2018-06-03 20:32:59 +0530

    Issue Fixes

--- ide/routing.py
+++ ide/routing.py
@@ -1,5 +1,5 @@
-from channels.routing import route, route_class
-from channels.staticfiles import StaticFilesConsumer
+from channels.routing import route, route_class      # noqa: ignore=F405
+from channels.staticfiles import StaticFilesConsumer # noqa: ignore=F405
 
 # routes defined for channel calls
 # this is similar to the Django urls, but specifically for Channels
