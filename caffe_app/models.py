17974a5468e1edb92d67e79d1445106f2f17df6f
Ram81 <ramramrakhya81@gmail.com>
2018-06-05 15:41:55 +0530

    Add Supporting Models

--- caffe_app/models.py
+++ caffe_app/models.py
@@ -2,6 +2,35 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.postgres.fields import JSONField
+from django.contrib.auth.models import User
+
+
+class Network(models.Model):
+    id = models.CharField(max_length=20, primary_key=True)
+    name = models.CharField(max_length=100)
+    network = JSONField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+    author = models.ForeignKey(User, blank=True, null=True)
+
+    def __unicode__(self):
+        return self.name
+
+
+class SharedWith(models.Model):
+    ACCESS_PRIVILEGE = (
+        ('E', 'Can Edit'),
+        ('V', 'Can View'),
+        ('C', 'Can Comment')
+    )
+    network = models.ForeignKey(Network)
+    user = models.ForeignKey(User)
+    access_privilege = models.CharField(max_length=1, choices=ACCESS_PRIVILEGE)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now_add=True)
+
+    def __unicode__(self):
+        return ''
 
 
 class ModelExport(models.Model):
