07ba03ec6ddbb07fc76280fb0373ff4ca7123739
Ram81 <ramramrakhya81@gmail.com>
2018-06-24 17:15:39 +0530

    Fixes in login validation

--- backendAPI/views.py
+++ backendAPI/views.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 from django.views.generic import TemplateView
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 
 # Create your views here.
@@ -10,14 +11,22 @@ class login(TemplateView):
 
 
 def check_login(request):
-    user = request.user
-    user_id = user.id
-    username = ''
-    is_authenticated = request.user.is_authenticated()
-    if (is_authenticated):
-        username = user.username
-    return JsonResponse({
-        'result': is_authenticated,
-        'user_id': user_id,
-        'username': username
-    })
+    try:
+        user = User.objects.get(username=request.user.username)
+        user_id = user.id
+        username = ''
+
+        is_authenticated = user.is_authenticated()
+        if (is_authenticated):
+            username = user.username
+
+        return JsonResponse({
+            'result': is_authenticated,
+            'user_id': user_id,
+            'username': username
+        })
+    except Exception as e:
+        return JsonResponse({
+            'result': False,
+            'error': str(e)
+        })
