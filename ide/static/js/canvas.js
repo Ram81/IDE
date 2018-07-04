34f6b0491b2913959add694c44bac3c927ee6dea
Ram81 <ramramrakhya81@gmail.com>
2018-07-04 23:50:41 +0530

    Initialization for RTC Commenting

--- ide/static/js/canvas.js
+++ ide/static/js/canvas.js
@@ -236,10 +236,12 @@ class Canvas extends React.Component {
     event.preventDefault();
   }
   clickLayerEvent(event, layerId) { // happens when layer is clicked and also dragged
-    if (this.clickOrDraggedLayer === 0) {
-      this.props.changeSelectedLayer(layerId); // clicked
-    } else if (this.clickOrDraggedLayer === 1) {
-      this.clickOrDraggedLayer = 0; // dragged
+    if (event.target.tagName.toLowerCase() == 'div') {
+      if (this.clickOrDraggedLayer === 0) {
+        this.props.changeSelectedLayer(layerId); // clicked
+      } else if (this.clickOrDraggedLayer === 1) {
+        this.clickOrDraggedLayer = 0; // dragged
+      }
     }
     event.stopPropagation();
   }
--- ide/static/js/layer.js
+++ ide/static/js/layer.js
@@ -2,6 +2,14 @@ import React from 'react';
 import data from './data';
 
 class Layer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      modalIsOpen: false,
+      layerId: 0
+    }
+    this.onAddComment = this.onAddComment.bind(this);
+  }
   componentDidMount() {
     instance.addLayerEndpoints(this.props.id,
       data[this.props.type].endpoint.src,
@@ -11,6 +19,10 @@ class Layer extends React.Component {
   componentWillUnmount() {
     instance.deleteEndpoint(`${this.props.id}-s0`);
     instance.deleteEndpoint(`${this.props.id}-t0`);
+  }
+  onAddComment(event, layerId) {
+    //console.log('ola')
+    this.setState({ modalIsOpen: true, layerId: layerId })
   }
   render() {
     return (
@@ -28,6 +40,14 @@ class Layer extends React.Component {
         data-tip='tooltip'
         data-for='getContent'
       >
+        <a style={{
+          position: 'absolute',
+          top: '-5px',
+          right: '-2px',
+          color: 'white'
+        }} onClick={(event) => this.onAddComment(event, this.props.id)}>
+          <span className="glyphicon glyphicon-plus-sign" style={{ fontSize: '15px', paddingRight: '5px'}} aria-hidden="true"></span>
+        </a>
         {data[this.props.type].name}
       </div>
     );
