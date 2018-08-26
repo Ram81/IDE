eb61aa874f270688fc111182f0ad3c449d1d60a6
Ram81 <ramramrakhya81@gmail.com>
2018-08-26 15:18:40 +0530

    Dockerized celery setup

--- docker/dev/django/container-start.sh
+++ docker/dev/django/container-start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd /code && \
 python manage.py migrate --noinput && \
-python manage.py runserver 0.0.0.0:8000
+python manage.py runserver 0.0.0.0:8000 & celery -A ide worker --app=ide.celery_app --loglevel=info
