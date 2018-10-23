6248817fdc27d8cc44b753862ae7d3c04c4cddca
Ram Ramrakhya <ram.ramrakhya@inmobi.com>
2018-10-23 23:54:30 +0530

    Fix TravisCI checks

--- ide/asgi.py
+++ ide/asgi.py
@@ -4,8 +4,9 @@ defined in the ASGI_APPLICATION setting.
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 from channels.asgi import get_channel_layer
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings")
 
 channel_layer = get_channel_layer()
